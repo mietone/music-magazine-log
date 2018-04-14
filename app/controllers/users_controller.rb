d80aa3718d95315e7ea065ffc87b97e79d70f449
Miwa <paintfx@gmail.com>
2018-04-14 00:59:47 +0000

    add logout and slug route to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,4 +36,19 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      # message "You have successfully logged out."
+      redirect "/login"
+    else
+      redirect "/"
+    end
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
+  end
+
 end
