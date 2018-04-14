8532e221537f0d44a067b4a4ff5db2c0fa2ba596
Miwa <paintfx@gmail.com>
2018-04-14 00:34:31 +0000

    add login routes to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,26 @@ class UsersController < ApplicationController
       session[:user_id] == user.id
       redirect "/users/#{user.slug}"
     else
-      redirect "/users/signup"
+      redirect "/signup"
     end
   end
 
+  get '/login' do
+    if logged_in?
+      redirect "/magazines"
+    else
+      erb :"/users/login"
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/magazines"
+    else
+      redirect "/signup"
+    end
+  end
 
 end
